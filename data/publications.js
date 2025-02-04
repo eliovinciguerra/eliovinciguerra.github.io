b21fce258212d6c44d5d435a059e03b862f19027
Elio <120382560+eliovinciguerra@users.noreply.github.com>
2025-02-05 00:02:35 +0100

    Initial release

---
+++ data/publications.js
@@ -0,0 +1,32 @@
+const publications = [
+  {
+      year: 2025,
+      title: 'Resource-Efficient Deployment of Reinforcement Learning-based MAC Protocols for Industrial IoT Networks',
+      reference: 'L. Miuccio, S. Riolo, E. Russo, E. Vinciguerra, G. Ascia, M. Palesi, D. Panno',
+      status: 'Under review'
+  },
+  {
+      year: 2025,
+      title: 'An Anomaly Detection Model for RISC-V in Automotive Applications: A Domain-Specific Accelerator Perspective',
+      reference: 'E. Vinciguerra, E. Russo, M. Palesi, G. Ascia',
+      status: 'To be presented'
+  },
+  {
+      year: 2024,
+      title: 'Data-Driven Simulation Based Fault Detection in Automotive RISC-V Applications',
+      reference: 'E. Vinciguerra, E. Russo, M. Palesi and G. Ascia, "Data-Driven Simulation Based Fault Detection in Automotive RISC-V Applications," 2024 IEEE 17th International Symposium on Embedded Multicore/Many-core Systems-on-Chip (MCSoC), Kuala Lumpur, Malaysia, 2024, pp. 509-516, doi: 10.1109/MCSoC64144.2024.00089.',
+      status: ''
+  },
+  {
+      year: 2024,
+      title: 'DNN Hardware Accelerator Selection for Feasible Deployment of MARL-Based MAC Protocols in Industrial IoT Networks',
+      reference: 'E. Russo, E. Vinciguerra, G. Ascia and D. Panno, "DNN Hardware Accelerator Selection for Feasible Deployment of MARL-Based MAC Protocols in Industrial IoT Networks," 2024 IEEE 8th Forum on Research and Technologies for Society and Industry Innovation (RTSI), Milano, Italy, 2024, pp. 443-448, doi: 10.1109/RTSI61910.2024.10761172.',
+      status: ''
+  },
+  {
+      year: 2024,
+      title: 'Improving LSTM-based Indoor Positioning via Simulation-Augmented Geomagnetic Field Dataset',
+      reference: 'E. Vinciguerra, E. Russo, M. Palesi, G. Ascia and H. Rafique, "Improving LSTM-based Indoor Positioning via Simulation-Augmented Geomagnetic Field Dataset," 2024 IEEE International Conference on Mobility, Operations, Services and Technologies (MOST), Dallas, TX, USA, 2024, pp. 251-259, doi: 10.1109/MOST60774.2024.00034.',
+      status: ''
+  }
+];
