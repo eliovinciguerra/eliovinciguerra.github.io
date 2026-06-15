1a7624fb62746518e5b17591961c029f4f958fb6
Elio <120382560+eliovinciguerra@users.noreply.github.com>
2026-06-15 20:32:51 +0200

    Update

--- data/publications.js
+++ data/publications.js
@@ -1,44 +1,64 @@
 const publications = [
   {
     year: 2026,
-    title: 'CHAOS: Controlled Hardware fAult injectOr System for gem5',
-    reference: 'E. Vinciguerra, E. Russo, G. Ascia, and M. Palesi, "CHAOS: Controlled hardware fault injector system for gem5", 2026. arXiv: 2602.02119 [cs.AR]. [Online]. Available: https://arxiv.org/abs/2602.02119.',
-    status: 'preprint',
+    title: "SAFER: Self-Attentive Feature Extraction and Reduction",
+    reference:
+      'E. Vinciguerra, G. Siracusano, G. Ascia and M. Palesi, "SAFER: Self-Attentive Feature Extraction and Reduction," in IEEE Access, vol. 14, pp. 85695-85708, 2026, doi: 10.1109/ACCESS.2026.3700810.',
+    status: "",
+  },
+  {
+    year: 2026,
+    title: "CHAOS: Controlled Hardware fAult injectOr System for gem5",
+    reference:
+      'E. Vinciguerra, E. Russo, G. Ascia, and M. Palesi, "CHAOS: Controlled hardware fault injector system for gem5", 2026. arXiv: 2602.02119 [cs.AR]. [Online]. Available: https://arxiv.org/abs/2602.02119.',
+    status: "preprint",
   },
   {
     year: 2025,
-    title: 'Improving Machine Learning Anomaly Detection for Safety-Critical RISC-V Automotive Systems',
-    reference: 'E. Vinciguerra, M. Palesi, and G. Ascia, "Improving Machine Learning Anomaly Detection for Safety-Critical RISC-V Automotive Systems," in Proceedings of the CPS Summer School PhD Workshop 2025, C. Pilato et al., Eds., vol. 4106. Aachen, Germany: CEUR-WS.org, 2025. [Online]. Available: https://ceur-ws.org/Vol-4106/short1.pdf.',
-    status: '',
+    title:
+      "Improving Machine Learning Anomaly Detection for Safety-Critical RISC-V Automotive Systems",
+    reference:
+      'E. Vinciguerra, M. Palesi, and G. Ascia, "Improving Machine Learning Anomaly Detection for Safety-Critical RISC-V Automotive Systems," in Proceedings of the CPS Summer School PhD Workshop 2025, C. Pilato et al., Eds., vol. 4106. Aachen, Germany: CEUR-WS.org, 2025. [Online]. Available: https://ceur-ws.org/Vol-4106/short1.pdf.',
+    status: "",
   },
   {
     year: 2025,
-    title: 'TeleSABRE: Layout Synthesis in Multi-Core Quantum Systems with Teleport Interconnect',
-    reference: 'E. Russo, E. Vinciguerra, M. Palesi, D. Patti, G. Ascia and V. Catania, "TeleSABRE: Heuristic Layout Synthesis in Multi-Core Quantum Systems with Teleport Interconnect," 2025 IEEE International Conference on Quantum Computing and Engineering (QCE), Albuquerque, NM, USA, 2025, pp. 749-758, doi: 10.1109/QCE65121.2025.00086.',
-    status: ''
+    title:
+      "TeleSABRE: Layout Synthesis in Multi-Core Quantum Systems with Teleport Interconnect",
+    reference:
+      'E. Russo, E. Vinciguerra, M. Palesi, D. Patti, G. Ascia and V. Catania, "TeleSABRE: Heuristic Layout Synthesis in Multi-Core Quantum Systems with Teleport Interconnect," 2025 IEEE International Conference on Quantum Computing and Engineering (QCE), Albuquerque, NM, USA, 2025, pp. 749-758, doi: 10.1109/QCE65121.2025.00086.',
+    status: "",
   },
   {
-      year: 2025,
-      title: 'An Anomaly Detection Model for RISC-V in Automotive Applications: A Domain-Specific Accelerator Perspective',
-      reference: 'E. Vinciguerra, E. Russo, M. Palesi, G. Ascia, "An Anomaly Detection Model for RISC-V in Automotive Applications: A Domain-Specific Accelerator Perspective," 2025 33rd Euromicro International Conference on Parallel, Distributed and Network-Based Processing (PDP), Turin, Italy, 2025, pp. 317-320, doi: 10.1109/PDP66500.2025.00051.',
-      status: ''
+    year: 2025,
+    title:
+      "An Anomaly Detection Model for RISC-V in Automotive Applications: A Domain-Specific Accelerator Perspective",
+    reference:
+      'E. Vinciguerra, E. Russo, M. Palesi, G. Ascia, "An Anomaly Detection Model for RISC-V in Automotive Applications: A Domain-Specific Accelerator Perspective," 2025 33rd Euromicro International Conference on Parallel, Distributed and Network-Based Processing (PDP), Turin, Italy, 2025, pp. 317-320, doi: 10.1109/PDP66500.2025.00051.',
+    status: "",
   },
   {
-      year: 2024,
-      title: 'Data-Driven Simulation Based Fault Detection in Automotive RISC-V Applications',
-      reference: 'E. Vinciguerra, E. Russo, M. Palesi and G. Ascia, "Data-Driven Simulation Based Fault Detection in Automotive RISC-V Applications," 2024 IEEE 17th International Symposium on Embedded Multicore/Many-core Systems-on-Chip (MCSoC), Kuala Lumpur, Malaysia, 2024, pp. 509-516, doi: 10.1109/MCSoC64144.2024.00089.',
-      status: ''
+    year: 2024,
+    title:
+      "Data-Driven Simulation Based Fault Detection in Automotive RISC-V Applications",
+    reference:
+      'E. Vinciguerra, E. Russo, M. Palesi and G. Ascia, "Data-Driven Simulation Based Fault Detection in Automotive RISC-V Applications," 2024 IEEE 17th International Symposium on Embedded Multicore/Many-core Systems-on-Chip (MCSoC), Kuala Lumpur, Malaysia, 2024, pp. 509-516, doi: 10.1109/MCSoC64144.2024.00089.',
+    status: "",
   },
   {
-      year: 2024,
-      title: 'DNN Hardware Accelerator Selection for Feasible Deployment of MARL-Based MAC Protocols in Industrial IoT Networks',
-      reference: 'E. Russo, E. Vinciguerra, G. Ascia and D. Panno, "DNN Hardware Accelerator Selection for Feasible Deployment of MARL-Based MAC Protocols in Industrial IoT Networks," 2024 IEEE 8th Forum on Research and Technologies for Society and Industry Innovation (RTSI), Milano, Italy, 2024, pp. 443-448, doi: 10.1109/RTSI61910.2024.10761172.',
-      status: ''
+    year: 2024,
+    title:
+      "DNN Hardware Accelerator Selection for Feasible Deployment of MARL-Based MAC Protocols in Industrial IoT Networks",
+    reference:
+      'E. Russo, E. Vinciguerra, G. Ascia and D. Panno, "DNN Hardware Accelerator Selection for Feasible Deployment of MARL-Based MAC Protocols in Industrial IoT Networks," 2024 IEEE 8th Forum on Research and Technologies for Society and Industry Innovation (RTSI), Milano, Italy, 2024, pp. 443-448, doi: 10.1109/RTSI61910.2024.10761172.',
+    status: "",
   },
   {
-      year: 2024,
-      title: 'Improving LSTM-based Indoor Positioning via Simulation-Augmented Geomagnetic Field Dataset',
-      reference: 'E. Vinciguerra, E. Russo, M. Palesi, G. Ascia and H. Rafique, "Improving LSTM-based Indoor Positioning via Simulation-Augmented Geomagnetic Field Dataset," 2024 IEEE International Conference on Mobility, Operations, Services and Technologies (MOST), Dallas, TX, USA, 2024, pp. 251-259, doi: 10.1109/MOST60774.2024.00034.',
-      status: ''
-  }
+    year: 2024,
+    title:
+      "Improving LSTM-based Indoor Positioning via Simulation-Augmented Geomagnetic Field Dataset",
+    reference:
+      'E. Vinciguerra, E. Russo, M. Palesi, G. Ascia and H. Rafique, "Improving LSTM-based Indoor Positioning via Simulation-Augmented Geomagnetic Field Dataset," 2024 IEEE International Conference on Mobility, Operations, Services and Technologies (MOST), Dallas, TX, USA, 2024, pp. 251-259, doi: 10.1109/MOST60774.2024.00034.',
+    status: "",
+  },
 ];
